8d75241beac1ff2e43fbe15a9d6e37675c61116a
mio818 <132991761+mio818@users.noreply.github.com>
2023-10-28 11:31:41 +0900

    Wifi接続

--- hardware/main.py
+++ hardware/main.py
@@ -1,5 +1,23 @@
 from machine import Pin
 import time
+import network
 
 trigger = Pin(14,Pin.OUT)
 echo = Pin(15,Pin.IN)
+
+ssid = 'にんじん🥕'
+password = 'mmmiii88'
+
+def connect():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+
+    if wlan.status() != 3:
+        raise RuntimeError('ネットワーク接続失敗')
+    else:
+        print('接続完了')
+        status = wlan.ifconfig()
+        print( 'IPアドレス = ' + status[0] )
+
+connect()
