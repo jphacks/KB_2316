02bd44ab1b4b48080bb36b66f2ba324d1d787524
lion-rion <lionact08@gmail.com>
2023-10-28 11:14:40 +0900

    add : sqliteテスト成功

---
+++ backend/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+
+	var err error
+	_, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("DB connect Error " + err.Error())
+	}
+}
