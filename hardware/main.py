412de02efa1eec1ee01e62529301914422dd2603
mio818 <is0720hi@ed.ritsumei.ac.jp>
2023-10-28 23:20:02 +0900

    少し訂正

--- hardware/main.py
+++ hardware/main.py
@@ -1,14 +1,15 @@
 from machine import Pin
-import time
+import utime
 import network
 import urequests
 
-trigger = Pin(14,Pin.OUT)
-echo = Pin(15,Pin.IN)
+trigger = Pin(14, Pin.OUT)
+echo = Pin(15, Pin.IN)
 
-ssid = 'にんじん🥕'
-password = 'mmmiii88'
-url ="http://133.242.18.204:8080/api/v1/record/b7f0542f"
+ssid = "にんじん🥕"
+password = "mmmiii88"
+url = "http://133.242.18.204:8080/api/v1/record/b7f0542f"
+
 
 def connect():
     wlan = network.WLAN(network.STA_IF)
@@ -16,19 +17,19 @@ def connect():
     wlan.connect(ssid, password)
 
     if wlan.status() != 3:
-        raise RuntimeError('ネットワーク接続失敗')
+        raise RuntimeError("ネットワーク接続失敗")
     else:
-        print('接続完了')
+        print("接続完了")
         status = wlan.ifconfig()
-        print( 'IPアドレス = ' + status[0] )
+        print("IPアドレス = " + status[0])
 
-] )
 
 def send_post_request():
-    #POST リクエストを送信する場所
+    # POST リクエストを送信する場所
     response = urequests.post(url)
     print(response.text)
-        
+
+
 def read_distance():
     trigger.low()
     utime.sleep_us(2)
@@ -41,7 +42,8 @@ def read_distance():
         signalon = utime.ticks_us()
     timepassed = signalon - signaloff
     distance = (timepassed * 0.0343) / 2
-    print("dinstance: ",distance,"cm")
+    print("dinstance: ", distance, "cm")
+
 
 def main():
     connect()
@@ -50,11 +52,12 @@ def main():
         utime.sleep(0.1)
         val2 = read_distance()
         utime.sleep(0.1)
-        
-        if val1-val2 >5  or val1-val2 < -5:
+
+        if val1 - val2 > 5 or val1 - val2 < -5:
             print("5cm 以上の差分が出ました")
             send_post_request()
             utime.sleep(600)
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     main()
