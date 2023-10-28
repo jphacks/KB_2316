fc17d3b91f689e3a48880d87d24c5cc22ce0561e
mio818 <132991761+mio818@users.noreply.github.com>
2023-10-28 11:43:22 +0900

    超音波センサで距離を測る

--- hardware/main.py
+++ hardware/main.py
@@ -20,4 +20,24 @@ def connect():
         status = wlan.ifconfig()
         print( 'IPアドレス = ' + status[0] )
 
+] )
+        
+def read_distance():
+    trigger.low()
+    utime.sleep_us(2)
+    trigger.high()
+    utime.sleep(0.00001)
+    trigger.low()
+    while echo.value() == 0:
+        signaloff = utime.ticks_us()
+    while echo.value() == 1:
+        signalon = utime.ticks_us()
+    timepassed = signalon - signaloff
+    distance = (timepassed * 0.0343) / 2
+    print("dinstance: ",distance,"cm")
+
 connect()
+
+while True:
+   read_distance()
+   utime.sleep(0.1)
