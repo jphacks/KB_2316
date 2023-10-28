ee1ff9fe7bbf6a4978ceecc2c6538cfbaa372fa9
lion-rion <lionact08@gmail.com>
2023-10-28 16:39:24 +0900

    update: CreateRecordを作成

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"net/http"
 	"r/models"
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
+
+var db *gorm.DB
 
 func init() {
 	models.InitDB()
@@ -26,5 +29,23 @@ func main() {
 		return c.String(200, "OK")
 	})
 
+	g := e.Group("/api/v1")
+
+	g.POST("/record/:uuid", CreateRecord)
+
 	e.Start(":1991")
+}
+
+func CreateRecord(c echo.Context) error {
+	var d models.Count
+	d.UUID = c.Param("uuid")
+
+	if err := db.Create(&d).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{
+			"message": "Error occurred",
+			"error":   err.Error(),
+		})
+	}
+
+	return c.JSON(http.StatusOK, d)
 }
